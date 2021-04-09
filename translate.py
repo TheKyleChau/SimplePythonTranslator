4c058f3db0d0d6043a6a47db6f7a3897e90e7de2
EpicJimmy5 <theclone1000@gmail.com>
2021-04-08 23:28:36 -0700

    v0.2-alpha: Added Yandex and Bing to choose which translation API

--- translate.py
+++ translate.py
@@ -1,15 +1,30 @@
 import translators as ts
 
 #CLI-Version
-#v0.1
+#v0.2-alpha
 #I've been testing this out for a while but it seems okay for now
 #Uses the translators library with Google Translation API
 #Should be okay for regular usage
+#Adding usage for multiple translation APIs
 
 print("Welcome to the CLI translator")
 i = 0
+translator = ''
 while i == 0:
+    valid = 0
     exit = 0
+    while valid == 0:
+        print("Enter translator (Google, Yandex, Bing)")
+        translator = input()
+        translator = translator.lower()
+        if translator == 'google':
+            valid = 1
+        elif translator == 'yandex':
+            valid = 1
+        elif translator == 'bing':
+            valid = 1
+        else:
+            print("Translator not valid.")
     print("Enter your input language (2 Character Abbreviation or Auto): ")
     inputlang = input()
     print()
@@ -19,7 +34,12 @@ while i == 0:
     print("Enter output language (2 Character Abbreviation): ")
     outputlang = input()
     print()
-    print("Output: " + ts.google(entry, from_language = inputlang,to_language = outputlang))
+    if translator == 'google':
+        print("Output: " + ts.google(entry, from_language = inputlang,to_language = outputlang))
+    if translator == 'bing':
+        print("Output: " + ts.bing(entry, from_language = inputlang,to_language = outputlang))
+    if translator == 'yandex':
+        print("Output: " + ts.yandex(entry, from_language = inputlang,to_language = outputlang))
     print()
     print("Are you finished using the translator tool? Enter Y/N: ")
     x = input()
